36bddfa69e2d0ebe3621545c0048ad97ad3e103e
Chauhan Kirankumar <kirankumar.chauhan@tieto.com>
2018-08-27 18:54:17 +0530

    Template driven form added

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -8,19 +6,6 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  itemValue = '';
-  items: Observable<any[]>;
-
-  constructor(public db: AngularFireDatabase) {
-    this.items = db.list('items').valueChanges();
-  }
-
-  onSubmit() {
-    this.db.list('/items').push({ content: this.itemValue });
-    this.itemValue = '';
-  }
-
   ngOnInit() {
   }
 
--- src/app/second-page/second-page.component.ts
+++ src/app/second-page/second-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-second-page',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-page.component.scss']
 })
 export class SecondPageComponent implements OnInit {
+  itemValue = '';
+  items: Observable<any[]>;
 
-  constructor() { }
+  constructor(public db: AngularFireDatabase) {
+    this.items = db.list('items').valueChanges();
+  }
+
+  onSubmit() {
+    this.db.list('/items').push({ content: this.itemValue });
+    this.itemValue = '';
+  }
 
   ngOnInit() {
   }
