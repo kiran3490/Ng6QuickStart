e5972d66d0d4e5160cd96a962261e827d67bcb60
Chauhan Kirankumar <kirankumar.chauhan@tieto.com>
2019-02-05 19:30:46 +0530

    removed sensitivve info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,7 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'AIzaSyDWCwBK6MmF_PF0i45z0dna-rc5S6ZLJHQ',
-        authDomain: 'ang4dbkiran.firebaseapp.com',
-        databaseURL: 'https://ang4dbkiran.firebaseio.com',
-        projectId: 'ang4dbkiran',
-        storageBucket: 'ang4dbkiran.appspot.com',
-        messagingSenderId: '199953370321'
+
     }
 };
 
